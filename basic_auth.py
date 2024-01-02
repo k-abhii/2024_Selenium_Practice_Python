ad81e403823c83e528b106e2d478c40dead54835
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2024-01-02 21:34:43 +0530

    By Pass Injection Authentication Popup

--- basic_auth.py
+++ basic_auth.py
@@ -5,9 +5,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 service_obj = Service("C:\Program Files (x86)\chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
-driver.get("https://mypage.rediff.com/login/dologin")
-driver.find_element(By.CSS_SELECTOR,"input[value='Login']").click()
-time.sleep(2)
-alertwindow = driver.switch_to.alert
-print(alertwindow.text)
-alertwindow.accept()
+driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth")
+print(driver.title)
+print(driver.current_url)
+time.sleep(5)
+# INJECTION
+driver.maximize_window()
