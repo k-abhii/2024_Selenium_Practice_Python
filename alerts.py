ca20acd60095d958ed0570529e161d52a56b5351
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2024-01-02 20:33:18 +0530

    alert dismiss

--- alerts.py
+++ alerts.py
@@ -9,9 +9,15 @@ driver.get("https://the-internet.herokuapp.com/javascript_alerts")
 print(driver.title)
 print(driver.current_url)
 driver.find_element(By.CSS_SELECTOR,"button[onclick='jsPrompt()']").click()
-time.sleep(5)
+time.sleep(1)
 alertwindow = driver.switch_to.alert
 print(alertwindow.text)
 alertwindow.send_keys("Abhimanyu Kumar")
-alertwindow.accept()
-# alertwindow.dismiss()
+# alertwindow.accept()
+
+alertwindow.dismiss()
+time.sleep(2)
+
+# git remote add origin https://github.com/k-abhii/2024_Selenium_Practice_Python.git
+# git branch -M main
+# git push -u origin main
